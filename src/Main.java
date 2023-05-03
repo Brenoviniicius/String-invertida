3eaafd11db83b1e7221b15b4f8cc64fe69c8ae1c
Breno Vinicius <brenovmsilva82@gmail.com>
2023-05-02 22:54:29 -0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String original = "oculos";
+        String invertido = "";
+
+// Adiciona cada caractere da string original à nova string de trás para frente
+        for (int i = original.length() - 1; i >= 0; i--) {
+            invertido += original.charAt(i);
+        }
+
+// Exibe a string invertida
+        System.out.println(invertido);
+
     }
 }
